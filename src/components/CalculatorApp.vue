8108622102a50db5af214ae8017a627129e59d60
Fscrew <cylcenlynn@gmail.com>
2023-11-07 11:03:15 -0500

    Display input numbers on top of the grid

--- src/components/CalculatorApp.vue
+++ src/components/CalculatorApp.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="calculator-app">
+    <div class="display">{{ display }}</div>
     <div class="grid">
       <button
         v-for="number in numbers"
@@ -18,12 +19,14 @@ import { defineComponent, ref } from "vue";
 export default defineComponent({
   setup() {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+    const display = ref("");
     const inputNumber = (number: number) => {
       console.log(number);
+      display.value += number.toString();
     };
     return {
       numbers,
+      display,
       inputNumber,
     };
   },
@@ -31,6 +34,23 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.calculator-app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.display {
+  font-size: 2rem;
+  margin-bottom: 20px;
+  background-color: #f0f0f0;
+  padding: 10px;
+  width: 200px;
+  text-align: right;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
 .grid {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
