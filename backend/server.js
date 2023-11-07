c7ca29c9c1805e6ed5dc17bbecb8ca114b3e6147
Fscrew <cylcenlynn@gmail.com>
2023-11-07 11:02:56 -0500

    Trying to connect Front/Backends but wrote useless code in HelloWorld.vue

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-//sample api endpoint
+// Enable CORS
+app.use(cors());
+
+//API setting
+app.get("/api/hello", (req, res) => {
+  res.josn({ message: "API is set" });
+});
+
+//sample input
 app.get("/", (req, res) => {
   console.log("Hello World YEAH"); //log the sentence to the terminal
   res.send("Hell YEAH from the backend");
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,108 +1,32 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div>
+    <h1>Dara from the Backend</h1>
+    <p>{{ message }}</p>
+    <p></p>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script>
+import axios from "axios";
 
-export default defineComponent({
-  name: "HelloWorld",
-  props: {
-    msg: String,
+export default {
+  data() {
+    return {
+      message: "Waiting for response from the server...",
+    };
   },
-});
+  mounted() {
+    axios
+      .get("http://localhost:3000/api/hello") //Backend URL
+      .then((response) => {
+        this.message = response.data.message;
+      })
+      .catch((error) => {
+        console.error("Error fetching data from the server:", error);
+        this.message = "Error fetching data";
+      });
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
