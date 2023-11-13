41af6dee67391d976a8d17cf93fa0e7bc9c714d6
Fscrew <cylcenlynn@gmail.com>
2023-11-13 11:05:50 -0500

    Fixed missing cors issue

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,9 @@ import { performCalculation } from "./utils/calculatorUtils";
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
